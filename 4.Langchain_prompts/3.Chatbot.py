d1247e7fda2fbc4f5a4de112fa2cf2e8f119d45b
Aaaaddddyyyyyyyy <iadityajha693@gmail.com>
2026-07-18 22:51:34 +0530

    Developed contextual thinking in chatbot

--- 4.Langchain_prompts/3.Chatbot.py
+++ 4.Langchain_prompts/3.Chatbot.py
@@ -6,10 +6,13 @@ load_dotenv()
 
 model= ChatGoogleGenerativeAI(model="gemini-3.5-flash")
 
+chat_history= []        ## stores the context for bot
 while True:
     user_input= input("You: ")  ## text_input takes input from user
+    chat_history.append(user_input)
     if user_input=='exit':
         break
     result=model.invoke(user_input)
+    chat_history.append(result.text())
 
     print("AdGPT",result.text())

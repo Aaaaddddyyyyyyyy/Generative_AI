9649e14e88e3ec7811b2f13286944691f87e0ce4
Aaaaddddyyyyyyyy <iadityajha693@gmail.com>
2026-07-20 09:13:27 +0530

    chaim

---
+++ 7.Chains_langchain/3.Parallel_chain.py
@@ -0,0 +1,27 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_groq import ChatGroq
+
+load_dotenv()
+
+model1=ChatGoogleGenerativeAI(model='"gemini-3.5-flash"')
+model2= ChatGroq(model="llama-3.3-70b-versatile")
+
+prompt1= PromptTemplate(
+    template='genrate short and simple notes from the following text \n {text}',
+    input_variables=['text']
+)
+
+
+prompt2= PromptTemplate(
+    template="generate 5 short question answer from text \n {text}",
+    input_variables=['text']
+)
+
+prompt3= PromptTemplate(
+    tempalte='merge the provided notes and quiz  into  a single document \n notes ->{notes} and quiz -> {quiz}',
+    input_variables=['notes','quiz']
+)
+

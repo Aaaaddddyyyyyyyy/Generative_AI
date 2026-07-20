8c39a5e627be0f9cf5873da86d9921e43a55c048
Aaaaddddyyyyyyyy <iadityajha693@gmail.com>
2026-07-20 22:36:31 +0530

    conditional

--- 7.Chains_langchain/4.Conditiona_chain.py
+++ 7.Chains_langchain/4.Conditiona_chain.py
@@ -9,3 +9,17 @@ from langchain_core.runnables import RunnablePassthrough
 from langchain_core.runnables import RunnableSequence
 
 load_dotenv()
+
+
+model1=ChatGoogleGenerativeAI(model="gemini-3.5-flash")
+
+parser=StrOutputParser()
+
+prompt1=PromptTemplate(
+    template='classify the sentiment of following feedback text into positive or negative \n {feedback}',
+    input_variables=['feedback']
+)
+
+classifier_chain= prompt1 | model1 | parser
+
+print(classifier_chain.invoke({'feedback':'this is the terrible smartphone'}))

032dab4997f2efc000fd79e94719f79cb141c30a
Aaaaddddyyyyyyyy <iadityajha693@gmail.com>
2026-07-24 08:53:52 +0530

    text_loader_rag

--- 9.RAG/text_loader.py
+++ 9.RAG/text_loader.py
@@ -1,8 +1,14 @@
 from langchain_community.document_loaders import TextLoader
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
 
-loader= TextLoader('D:\Langchain_models\requirements.txt',encoding='utf-8')
+load_dotenv()
+
+loader= TextLoader('9.RAG\demo.txt',encoding='utf-8')
 
 docs=loader.load()       # load text file into document
 
-print(docs)
+#print(docs)
 print(docs[0])

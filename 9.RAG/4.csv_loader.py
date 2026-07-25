6bdb6d26f92c35db228b2e109223511487e946c4
Aaaaddddyyyyyyyy <iadityajha693@gmail.com>
2026-07-25 13:32:16 +0530

    text_spliter

--- 9.RAG/4.csv_loader.py
+++ 9.RAG/4.csv_loader.py
@@ -5,4 +5,4 @@ loader= CSVLoader(file_path='9.RAG\heart.csv')
 docs=loader.load()
 
 print(docs[1])
-print(len(docs))
+print(len(docs)) 
--- 9.RAG/5.text_spliter_len_based.py
+++ 9.RAG/5.text_spliter_len_based.py
@@ -1,4 +1,9 @@
 from langchain_text_splitters import CharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+
+loader= PyPDFLoader('9.RAG\ai.pdf')
+
+docs=loader.load()
 
 
 text="""A common mistake is treating the symptom (noise) with spray instead of fixing the worn part. If the noise has started recently, it's more likely that cleaning or replacing a worn component is the real solution.
@@ -10,6 +15,7 @@ splitter=CharacterTextSplitter(
         separator=''
 )
 
-result=splitter.split_text(text)
+#result=splitter.split_text(text)
 
+result=splitter.split_documents(docs)
 print(result)

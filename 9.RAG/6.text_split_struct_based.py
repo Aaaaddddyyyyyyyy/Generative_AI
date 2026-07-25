82e675b6e8ba9782f9d2008faf77df8ea044bf88
Aaaaddddyyyyyyyy <iadityajha693@gmail.com>
2026-07-25 15:01:51 +0530

    text_split_struc_based

---
+++ 9.RAG/6.text_split_struct_based.py
@@ -0,0 +1,11 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+text="""
+In Retrieval-Augmented Generation (RAG), text structure–based splitting is a strategy for chunking documents into meaningful segments before embedding and storing them in a vector database. Instead of splitting purely by token count or fixed length, this method leverages the inherent structure of the text (like headings, paragraphs, or semantic boundaries) to preserve context.
+
+"""
+
+# initialize the splitter
+splitter= RecursiveCharacterTextSplitter(
+    
+)

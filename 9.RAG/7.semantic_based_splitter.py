fc46d01c1471dcfb9a88c9df2dc1ed9000004b1c
Aaaaddddyyyyyyyy <iadityajha693@gmail.com>
2026-07-25 19:51:51 +0530

    semantic_based_splitter

---
+++ 9.RAG/7.semantic_based_splitter.py
@@ -0,0 +1,5 @@
+from langchain_experimental.text_splitter import SemanticChunker
+from langchain_google_genai.embeddings import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+
+load_dotenv()
